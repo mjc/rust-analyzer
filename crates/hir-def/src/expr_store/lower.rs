026a05b7456a897ff0a232ce3d69daf6d4022674
Mika Cohen <mjc@kernel.org>
2026-08-16 07:04:55 -0600

    hir-def: freeze expression source maps

--- crates/hir-def/src/expr_store/lower.rs
+++ crates/hir-def/src/expr_store/lower.rs
@@ -864,7 +864,6 @@ impl<'db> ExprCollector<'db> {
         let id = self.store.lifetimes.alloc(lifetime_ref);
         let ptr = self.expander.in_file(node);
         self.store.lifetime_map_back.insert(id, ptr);
-        self.store.lifetime_map.insert(ptr, id);
         id
     }
 
